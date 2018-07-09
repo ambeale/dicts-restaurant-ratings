ef9af35934878c72f78965a7db59ab2c1a38dcae
Hackbright Student <no-reply@hackbrightacademy.com>
2018-07-09 16:31:00 -0700

    Ask user to add restaurant

--- ratings.py
+++ ratings.py
@@ -1,19 +1,22 @@
-
 def restaurant_ratings(file_ratings):
 	"""Restaurant rating lister."""
 	
 	ratings = open(file_ratings)
-	ratings_dict={}
+	ratings_dict = {}
+	
+	#Add restaurants and ratings to dict
 	for line in ratings:
 		pair = line.strip().split(":")
 		ratings_dict[pair[0]] = pair[1]
-	#print(ratings_dict)	
+	
+	new_restaurant = str.capitalize(input("Enter restaurant: "))
+	new_rating = input("Enter rating out of 5: ")
 
+	ratings_dict[new_restaurant] = new_rating
+
+	# Sort and print restaurant ratings
 	for item in sorted(ratings_dict):
 		print("{} is rated at {}.".format(item,ratings_dict[item]))
 
+
 restaurant_ratings("scores.txt")
-
-
-
-# put your code here
